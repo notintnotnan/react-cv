e9a1f4fe0dabb05dc3748ee69b1a63e977c68d56
cinicDiver <oscarframirezp@hotmail.com>
2023-10-04 17:44:46 -0500

    Added Home page view.

---
+++ src/views/Homepage.js
@@ -0,0 +1,9 @@
+import HeroSection from "../components/HeroSection";
+
+export default function Homepage() {
+  return (
+    <div className="home-container">
+      <HeroSection heroImgUrl="images/heroImg.jpg" />
+    </div>
+  );
+}
