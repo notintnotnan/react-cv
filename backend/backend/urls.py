d6053f9c1fa7b29bb88a921fb83025f2b40347ce
cinicDiver <oscarframirezp@hotmail.com>
2023-10-11 15:17:59 -0500

    Added the api/ path part to server urls to handle django requests with ngingx on server.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("visits/",include("visits.urls"))
+    path("api/visits/",include("visits.urls"))
 ]
