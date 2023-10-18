15d862c299ba6a0651cd8b41625a21cc547a8eaf
cinicDiver <oscarframirezp@hotmail.com>
2023-10-18 11:56:06 -0500

    Added city field to visits table.

--- backend/visits/models.py
+++ backend/visits/models.py
@@ -5,6 +5,7 @@ class Visitor(models.Model):
     first_visit = models.DateTimeField(auto_now_add=True)
     last_visit = models.DateTimeField(null=True,blank=True)
     visit_number = models.IntegerField(default=1)
-    country = models.CharField(max_length=50, null=True,blank=True)
+    country = models.CharField(max_length=100, null=True,blank=True)
+    city = models.CharField(max_length=100, null=True,blank=True)
     country_code = models.CharField(max_length=4, null=True, blank=True)
     tz_offset = models.IntegerField(null=True, blank=True)
